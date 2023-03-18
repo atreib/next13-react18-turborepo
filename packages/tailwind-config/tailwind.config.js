d030f9cd05509a9f4d7ba19a5968254b744934ce
Andre Treib <andre.schumacher.treib@gmail.com>
2023-03-17 23:58:24 -0300

    chore: quickstart turborepo with next + 1 package + global tailwind

---
+++ packages/tailwind-config/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./../../apps/web/**/*.{ts,tsx}",
+    "./../../packages/ui/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
